9882cd46e899868ecc1cdee6bfe4eb174e5706b1
Zhafran Sidik <126880782+filanarfahz@users.noreply.github.com>
2026-03-17 21:06:07 +0100

    updated the model.py to incorporate Brenda's code on network graph generation and added the stuff for the shortest path. updated the model_run too xx

--- EPA133a-G14-A3/model/model.py
+++ EPA133a-G14-A3/model/model.py
@@ -4,6 +4,9 @@ from mesa.space import ContinuousSpace
 from components import Source, Sink, SourceSink, Bridge, Link, Intersection
 import pandas as pd
 from collections import defaultdict
+import networkx as nx
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 # ---------------------------------------------------------------
@@ -55,7 +58,7 @@ class BangladeshModel(Model):
 
     step_time = 1
 
-    file_name = '../data/demo-4.csv'
+    file_name = '../data/demo-4_Copy.csv' #using the copy made for now
 
     def __init__(self, seed=None, x_max=500, y_max=500, x_min=0, y_min=0):
 
@@ -65,6 +68,7 @@ class BangladeshModel(Model):
         self.space = None
         self.sources = []
         self.sinks = []
+        self.G = nx.Graph() #added this
 
         self.generate_model()
 
@@ -96,14 +100,15 @@ class BangladeshModel(Model):
                 3. put the path in reversed order and reindex
                 4. add the path to the path_ids_dict so that the vehicles can drive backwards too
                 """
-                path_ids = df_objects_on_road['id']
-                path_ids.reset_index(inplace=True, drop=True)
-                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
-                self.path_ids_dict[path_ids[0], None] = path_ids
-                path_ids = path_ids[::-1]
-                path_ids.reset_index(inplace=True, drop=True)
-                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
-                self.path_ids_dict[path_ids[0], None] = path_ids
+                #Commenting this out
+                # path_ids = df_objects_on_road['id']
+                # path_ids.reset_index(inplace=True, drop=True)
+                # self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
+                # self.path_ids_dict[path_ids[0], None] = path_ids
+                # path_ids = path_ids[::-1]
+                # path_ids.reset_index(inplace=True, drop=True)
+                # self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
+                # self.path_ids_dict[path_ids[0], None] = path_ids
 
         # put back to df with selected roads so that min and max and be easily calculated
         df = pd.concat(df_objects_all)
@@ -114,6 +119,33 @@ class BangladeshModel(Model):
             df['lon'].max(),
             0.05
         )
+
+        # add graph generation just from Brenda's notebook
+        nodes = df.drop_duplicates(subset='id')
+        for _, row in nodes.iterrows():
+            self.G.add_node(
+                int(row["id"]),
+                road=row["road"],
+                pos=(row["lat"], row["lon"]),
+            )
+
+        # Add edges
+        df["from_id"] = df.groupby("road")["id"].shift(1)
+        df["to_id"] = df["id"]
+        edges = df.dropna(subset=["from_id"]).copy()
+
+        for _, row in edges.iterrows():
+            self.G.add_edge(
+                int(row["from_id"]),
+                int(row["to_id"]),
+                road=row["road"],
+                type=row["model_type"],
+                length=row["length"],
+                weight=row["length"]  # <-- Assigning the weight directly!
+            )
+
+        pos = nx.get_node_attributes(self.G, "pos")
+        labels = nx.get_edge_attributes(self.G, "weight")
 
         # ContinuousSpace from the Mesa package;
         # not to be confused with the SimpleContinuousModule visualization
@@ -157,6 +189,22 @@ class BangladeshModel(Model):
                     self.space.place_agent(agent, (x, y))
                     agent.pos = (x, y)
 
+    #New function to calculate the shortest path
+    def find_shortest_path(self, source, sink):
+        if (source, sink) in self.path_ids_dict:
+            return self.path_ids_dict[(source, sink)]
+        try:
+            path_list = nx.shortest_path(self.G, source=source, target=sink, weight='weight')
+            path_series = pd.Series(path_list)
+
+            self.path_ids_dict[(source, sink)] = path_series
+            self.path_ids_dict[(sink, source)] = pd.Series(path_list[::-1])
+            return path_series
+
+        except nx.NetworkXNoPath:
+            print(f"No path between {source} and {sink}")
+            return pd.Series([])
+
     def get_random_route(self, source):
         """
         pick up a random route given an origin
@@ -166,17 +214,34 @@ class BangladeshModel(Model):
             sink = self.random.choice(self.sinks)
             if sink is not source:
                 break
-        return self.path_ids_dict[source, sink]
+        #Update the return statement here
+        return self.find_shortest_path(source, sink)
 
-    # TODO
+        #return self.path_ids_dict[source, sink]
+
+    # TODO: just use the random route
     def get_route(self, source):
-        return self.get_straight_route(source)
+        return self.get_random_route(source)
 
-    def get_straight_route(self, source):
-        """
-        pick up a straight route given an origin
-        """
-        return self.path_ids_dict[source, None]
+    #Deprecate
+    # def get_straight_route(self, source):
+    #     """
+    #     pick up a straight route given an origin
+    #     """
+    #     return self.path_ids_dict[source, None]
+    # New function to create the graph
+    def draw_graph(self, save_png=False):
+        pos = nx.get_node_attributes(self.G, "pos")
+        plt.figure(figsize=(10, 10))
+        nx.draw(self.G, pos=pos, with_labels=True)
+        plt.title("Network Routing Map of N1 and N2 with side roads >25km")
+        if save_png:
+            image_path = '../data/bangladesh_network.png'
+            plt.savefig(image_path, dpi=300, bbox_inches='tight')
+            plt.close()  # Closes the figure to free up memory
+            print(f"Map successfully saved to: {image_path}")
+        else:
+            plt.show()
 
     def step(self):
         """
--- EPA133a-G14-A3/model/model_run.py
+++ EPA133a-G14-A3/model/model_run.py
@@ -8,10 +8,10 @@ from model import BangladeshModel
 # ---------------------------------------------------------------
 
 # run time 5 x 24 hours; 1 tick 1 minute
-run_length = 5 * 24 * 60
+#run_length = 5 * 24 * 60
 
 # run time 1000 ticks
-# run_length = 1000
+run_length = 1000
 
 seed = 1234567
 
@@ -23,3 +23,12 @@ print("SEED " + str(sim_model._seed))
 # One run with given steps
 for i in range(run_length):
     sim_model.step()
+
+
+#print out the paths to verify it manually or whatever
+for (source, sink), path_series in sim_model.path_ids_dict.items():
+        print(f"Route from {source} to {sink}:")
+        print(f"  -> Path: {path_series.tolist()}")
+        print("-" * 40)
+
+sim_model.draw_graph(save_png=True)
